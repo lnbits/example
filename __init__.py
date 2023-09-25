41fbd84b4102c629428187b4d006602443e2c23e
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-09-25 15:27:37 +0100

    allow custom path (#5)
    * allow custom path
    * update minversion to 0.11.0
    
    ---------
    
    Co-authored-by: dni ⚡ <office@dnilabs.com>

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import asyncio
 
 from fastapi import APIRouter
-from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -14,14 +13,13 @@ example_ext: APIRouter = APIRouter(prefix="/example", tags=["example"])
 example_static_files = [
     {
         "path": "/example/static",
-        "app": StaticFiles(packages=[("lnbits", "extensions/example/static")]),
         "name": "example_static",
     }
 ]
 
 
 def example_renderer():
-    return template_renderer(["lnbits/extensions/example/templates"])
+    return template_renderer(["example/templates"])
 
 
 from .tasks import wait_for_paid_invoices
