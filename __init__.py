9585f2a2c2066745e1fff8a40340eacea1fbacee
dni ⚡ <office@dnilabs.com>
2024-04-15 12:55:56 +0200

    fix: properly start/stop tasks (#8)
    https://github.com/lnbits/lnbits/issues/2411

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 import asyncio
 
 from fastapi import APIRouter
+from loguru import logger
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.tasks import create_permanent_unique_task
 
 db = Database("ext_example")
 
@@ -27,6 +28,15 @@ from .views import *  # noqa: F401,F403
 from .views_api import *  # noqa: F401,F403
 
 
+scheduled_tasks: list[asyncio.Task] = []
+
+def example_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
+
 def example_start():
-    loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = create_permanent_unique_task("ext_example", wait_for_paid_invoices)
+    scheduled_tasks.append(task)
