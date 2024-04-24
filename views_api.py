6bbc4927793bca44c510f1cbd3d15157a088e2d1
Arc <33088785+arcbtc@users.noreply.github.com>
2024-04-24 10:36:03 +0100

    Adds extended description and vetted readme (#11)
    * Adds extended description and vetted readme
    
    * make format

--- views_api.py
+++ views_api.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from loguru import logger
 from .models import Example
+from http import HTTPStatus
+import httpx
+from fastapi.exceptions import HTTPException
+from lnbits.decorators import get_key_type, WalletTypeInfo
 
 # views_api.py is for you API endpoints that could be hit by another service
 
@@ -10,7 +14,21 @@ example_ext_api = APIRouter(
 )
 
 
-@example_ext_api.get("/{example_data}", description="Example API endpoint")
+@example_ext_api.get("/test/{example_data}", description="Example API endpoint")
 async def api_example(example_data: str) -> Example:
     # Do some python things and return the data
     return Example(id="1", wallet=example_data)
+
+
+@example_ext_api.get("/vetted", description="Get the vetted extension readme")
+async def api_get_vetted(wallet: WalletTypeInfo = Depends(get_key_type)):
+    try:
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(
+                "https://raw.githubusercontent.com/lnbits/lnbits-extensions/main/README.md"
+            )
+            return resp.text
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e)
+        ) from e
