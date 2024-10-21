3056dc57605f057719d2575c8e17187821aa4991
dni ⚡ <office@dnilabs.com>
2024-10-21 14:14:17 +0200

    feat: update to v1.0.0
    rc5

---
+++ static/js/index.js
@@ -0,0 +1,86 @@
+const someMapObject = obj => {
+  obj._data = _.clone(obj)
+  obj.date = Quasar.date.formatDate(new Date(obj.time), 'YYYY-MM-DD HH:mm')
+  // here you can do something with the mapped data
+  return obj
+}
+
+window.app = Vue.createApp({
+  el: '#vue',
+  mixins: [windowMixin],
+  // Declare models/variables
+  data() {
+    return {
+      protocol: window.location.protocol,
+      location: '//' + window.location.hostname,
+      thingDialog: {
+        show: false,
+        data: {}
+      },
+      someBool: true,
+      splitterModel: 20,
+      exampleData: [],
+      tab: 'frameworks',
+      framworktab: 'fastapi',
+      usefultab: 'magicalg',
+      vettedData: ''
+    }
+  },
+  // Where functions live
+  methods: {
+    exampleFunction(data) {
+      LNbits.api
+        .request(
+          'GET', // Type of request
+          '/example/api/v1/test/', // URL of the endpoint
+          this.g.user.wallets[0].inkey, // Often endpoints require a key
+          data
+        )
+        .then(response => {
+          this.exampleData = response.data.map(someMapObject) // Often whats returned is mapped onto some model
+        })
+        // Error will be passed to the frontend
+        .catch(LNbits.utils.notifyApiError)
+    },
+    getVettedReadme() {
+      // This is a function that gets the vetted readme from the LNbits repo and converts it from makrdown to html.
+      LNbits.api
+        .request('GET', '/example/api/v1/vetted', this.g.user.wallets[0].inkey)
+        .then(response => {
+          this.vettedData = LNbits.utils.convertMarkdown(response.data)
+        })
+        .catch(LNbits.utils.notifyApiError)
+    },
+    async initWs() {
+      if (location.protocol !== 'http:') {
+        localUrl =
+          'wss://' +
+          document.domain +
+          ':' +
+          location.port +
+          '/api/v1/ws/32872r23g29'
+      } else {
+        localUrl =
+          'ws://' +
+          document.domain +
+          ':' +
+          location.port +
+          '/api/v1/ws/32872r23g29'
+      }
+      this.ws = new WebSocket(localUrl)
+      this.ws.addEventListener('message', async ({data}) => {
+        const res = data.toString()
+        document.getElementById('text-to-change').innerHTML = res
+      })
+    },
+    sendThingDialog() {
+      console.log(this.thingDialog)
+    }
+  },
+  // To run on startup
+  created() {
+    this.exampleFunction('lorum')
+    this.initWs()
+    this.getVettedReadme()
+  }
+})
--- views_api.py
+++ views_api.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 import httpx
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
-from lnbits.decorators import WalletTypeInfo, get_key_type
+from lnbits.decorators import require_invoice_key
 
 from .models import Example
 
@@ -21,15 +21,19 @@ async def api_example(example_data: str) -> Example:
     return Example(id="2", wallet=example_data)
 
 
-@example_ext_api.get("/vetted", description="Get the vetted extension readme")
-async def api_get_vetted(wallet: WalletTypeInfo = Depends(get_key_type)):
+@example_ext_api.get(
+    "/vetted",
+    description="Get the vetted extension readme",
+    dependencies=[Depends(require_invoice_key)],
+)
+async def api_get_vetted():
     try:
         async with httpx.AsyncClient() as client:
             resp = await client.get(
                 "https://raw.githubusercontent.com/lnbits/lnbits-extensions/main/README.md"
             )
             return resp.text
-    except Exception as e:
+    except Exception as exc:
         raise HTTPException(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e)
-        ) from e
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=f"{exc!s}"
+        ) from exc
